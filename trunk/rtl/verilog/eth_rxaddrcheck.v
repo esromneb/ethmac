613f375e5ac292a64e61cc4ebb002144669baa3e
mohor <mohor@6d336fd2-2a17-44c4-b403-220f6aeeb104>
2002-03-02 21:03:11 +0000

    MAC address recognition was not correct (bytes swaped).

--- trunk/rtl/verilog/eth_rxaddrcheck.v
+++ trunk/rtl/verilog/eth_rxaddrcheck.v
@@ -150,22 +150,22 @@ begin
     UnicastOK <= #Tp 1'b0;
   else
   if(RxCheckEn & ByteCntEq2)
-    UnicastOK <= #Tp   RxData[7:0] == MAC[7:0];
+    UnicastOK <= #Tp   RxData[7:0] == MAC[47:40];
   else
   if(RxCheckEn & ByteCntEq3)
-    UnicastOK <= #Tp ( RxData[7:0] == MAC[15:8]) & UnicastOK;
+    UnicastOK <= #Tp ( RxData[7:0] == MAC[39:32]) & UnicastOK;
   else
   if(RxCheckEn & ByteCntEq4)
-    UnicastOK <= #Tp ( RxData[7:0] == MAC[23:16]) & UnicastOK;
-  else
-  if(RxCheckEn & ByteCntEq5)
     UnicastOK <= #Tp ( RxData[7:0] == MAC[31:24]) & UnicastOK;
   else
+  if(RxCheckEn & ByteCntEq5)
+    UnicastOK <= #Tp ( RxData[7:0] == MAC[23:16]) & UnicastOK;
+  else
   if(RxCheckEn & ByteCntEq6)
-    UnicastOK <= #Tp ( RxData[7:0] == MAC[39:32])  & UnicastOK;
+    UnicastOK <= #Tp ( RxData[7:0] == MAC[15:8])  & UnicastOK;
   else
   if(RxCheckEn & ByteCntEq7)
-    UnicastOK <= #Tp ( RxData[7:0] == MAC[47:40]) & UnicastOK;
+    UnicastOK <= #Tp ( RxData[7:0] == MAC[7:0])   & UnicastOK;
   else
   if(RxEndFrm | RxAbort)
     UnicastOK <= #Tp 1'b0;
